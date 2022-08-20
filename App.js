7590bb674aa13125f7e83ec6e2a6786832c42fb0
YUVRAJ6336 <193116@nith.ac.in>
2022-08-20 21:14:52 +0530

    Dishrow updated , reduxjs toolkit added , implemented provider for redux in app.js , created store.js which can account for global state in redux, created basketslice.js in features which will keep track of data layer to add or remove items from basket using redux

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,12 +14,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <Provider store={store}>
     <TailwindProvider>
     <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
     </TailwindProvider>
+    </Provider>
     </NavigationContainer>
   );
 }
